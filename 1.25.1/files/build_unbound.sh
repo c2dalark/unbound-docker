65107e694f2c8cbbd3bd0a1819a5ac0e3cfd1b3e
Christopher Clark <c2dalark@users.noreply.github.com>
2026-07-18 10:19:16 -0400

    stubbing out flags for i386 and arm/v5, adjusting padding

--- 1.25.1/files/build_unbound.sh
+++ 1.25.1/files/build_unbound.sh
@@ -20,10 +20,14 @@ groupadd _unbound
 useradd -g _unbound -s /usr/sbin/nologin -d /opt/unbound _unbound
 if [ $TARGETPLATFORM == "linux/amd64" ]; then
     config_flags="--host=x86_64-pc-linux-gnu "
+elif [ $TARGETPLATFORM == "linux/i386" ]; then
+    config_flags="--host=i386-pc-linux-gnu "
 elif [ $TARGETPLATFORM == "linux/arm64"  ]; then
     config_flags="--host=armv64-pc-linux-gnu "
 elif [ $TARGETPLATFORM == "linux/arm/v7" ]; then
     config_flags="--host=armv7l-pc-linux-gnu "
+elif [ $TARGETPLATFORM == "linux/arm/v5" ]; then
+    config_flags="--host=armv5-pc-linux-gnu "
 else
     echo "Unsupported target/build arch!"
     exit 1
--- 1.25.1/files/install_dependencies.sh
+++ 1.25.1/files/install_dependencies.sh
@@ -3,12 +3,16 @@
 set -x -e
 
 if [ $TARGETPLATFORM == "linux/amd64" ] || [ $TARGETPLATFORM == "linux/arm64"  ]; then
-    dependencies="libevent-2.1-7 "
+  dependencies="libevent-2.1-7 "
 elif [ $TARGETPLATFORM == "linux/arm/v7" ]; then
-    dependencies="libevent-2.1-7t64 "
+  dependencies="libevent-2.1-7t64 "
+elif [ $TARGETPLATFORM == "linux/arm/v5" ]; then
+  dependencies="libevent-2.1-7t64 "
+elif [ $TARGETPLATFORM == "linux/i386" ]; then
+  dependencies="libevent-2.1-7 "
 else
-    echo "Unsupported target/build arch!"
-    exit 1
+  echo "Unsupported target/build arch!"
+  exit 1
 fi
 
 DEBIAN_FRONTEND=noninteractive apt-get update && apt-get upgrade -y && apt-get install -y --no-install-recommends \
