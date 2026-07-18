02de4e83e53ef3f01d568515aef05999364196c7
Christopher Clark <c2dalark@users.noreply.github.com>
2026-07-18 10:24:54 -0400

    additional padding adjustments

--- 1.25.1/files/build_unbound.sh
+++ 1.25.1/files/build_unbound.sh
@@ -19,32 +19,32 @@ cd unbound-1.25.1
 groupadd _unbound
 useradd -g _unbound -s /usr/sbin/nologin -d /opt/unbound _unbound
 if [ $TARGETPLATFORM == "linux/amd64" ]; then
-    config_flags="--host=x86_64-pc-linux-gnu "
+  config_flags="--host=x86_64-pc-linux-gnu "
 elif [ $TARGETPLATFORM == "linux/i386" ]; then
-    config_flags="--host=i386-pc-linux-gnu "
+  config_flags="--host=i386-pc-linux-gnu "
 elif [ $TARGETPLATFORM == "linux/arm64"  ]; then
-    config_flags="--host=armv64-pc-linux-gnu "
+  config_flags="--host=armv64-pc-linux-gnu "
 elif [ $TARGETPLATFORM == "linux/arm/v7" ]; then
-    config_flags="--host=armv7l-pc-linux-gnu "
+  config_flags="--host=armv7l-pc-linux-gnu "
 elif [ $TARGETPLATFORM == "linux/arm/v5" ]; then
-    config_flags="--host=armv5-pc-linux-gnu "
+  config_flags="--host=armv5-pc-linux-gnu "
 else
-    echo "Unsupported target/build arch!"
-    exit 1
+  echo "Unsupported target/build arch!"
+  exit 1
 fi
 ./configure \
-    --prefix=/opt/unbound \
-    --with-pthreads \
-    --with-username=_unbound \
-    --with-ssl=/opt/openssl \
-    --with-libevent \
-    --with-libnghttp2 \
-    --enable-dnstap \
-    --enable-tfo-server \
-    --enable-tfo-client \
-    --enable-event-api \
-    --enable-subnet \
-    $config_flags
+  --prefix=/opt/unbound \
+  --with-pthreads \
+  --with-username=_unbound \
+  --with-ssl=/opt/openssl \
+  --with-libevent \
+  --with-libnghttp2 \
+  --enable-dnstap \
+  --enable-tfo-server \
+  --enable-tfo-client \
+  --enable-event-api \
+  --enable-subnet \
+  $config_flags
 make install
 mv /opt/unbound/etc/unbound/unbound.conf /opt/unbound/etc/unbound/unbound.conf.example
 apt-get purge -y --auto-remove \
