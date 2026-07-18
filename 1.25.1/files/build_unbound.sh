dd1ab44352e2236d582a08939d10e9da6ff33c85
Christopher Clark <c2dalark@users.noreply.github.com>
2026-07-18 11:24:48 -0400

    Cross Compiling
    - Installs gcc packages for cross-compiling destations
    - updates --host flags for build to target those compilers

--- 1.25.1/files/build_unbound.sh
+++ 1.25.1/files/build_unbound.sh
@@ -2,8 +2,10 @@
 set -e -x
 
 build_deps="bison build-essential curl flex gcc libc-dev libevent-dev libexpat1-dev libnghttp2-dev make"
+cross_compilers="gcc-arm-linux-gnueabihf gcc-aarch64-linux-gnu cpp-i686-linux-gnu"
 apt-get update && apt-get install -y --no-install-recommends \
   $build_deps \
+  $cross_compilers \
   bsdmainutils \
   ca-certificates \
   ldnsutils \
@@ -19,15 +21,15 @@ cd unbound-1.25.1
 groupadd _unbound
 useradd -g _unbound -s /usr/sbin/nologin -d /opt/unbound _unbound
 if [ $TARGETPLATFORM == "linux/amd64" ]; then
-  config_flags="--host=x86_64-pc-linux-gnu "
+  config_flags="--host=x86_64-linux-gnu "
 elif [ $TARGETPLATFORM == "linux/i386" ]; then
-  config_flags="--host=i386-pc-linux-gnu "
+  config_flags="--host=i686-linux-gnu "
 elif [ $TARGETPLATFORM == "linux/arm64"  ]; then
-  config_flags="--host=armv64-pc-linux-gnu "
+  config_flags="--host=aarch64-linux-gnu "
 elif [ $TARGETPLATFORM == "linux/arm/v7" ]; then
-  config_flags="--host=armv7l-pc-linux-gnu "
+  config_flags="--host=armv7l-linux-gnu "
 elif [ $TARGETPLATFORM == "linux/arm/v5" ]; then
-  config_flags="--host=armv5-pc-linux-gnu "
+  config_flags="--host=armv5-linux-gnu "
 else
   echo "Unsupported target/build arch!"
   exit 1
