0229a7263aa24ce9a6fc516353c25c13ee1252d8
Christopher Clark <c2dalark@users.noreply.github.com>
2026-07-17 20:02:20 -0400

    Prep to test unbound build from script
    - Restores openssl_build stage to original
    - Moves unbound_build to script based process with logic using buildx
      multi-arch ARGS
    - Removes openssl.sh

--- 1.25.1/files/install_unbound.sh
+++ 1.25.1/files/install_unbound.sh
@@ -1,13 +1,47 @@
 #!/usr/bin/env bash
+set -e -x
 
-config_flags=""
-if [ $TARGETPLATFORM == "linux/amd64" ] || [ $TARGETPLATFORM == "linux/arm64"  ]; then
-    echo "Building configure flags for 64bit arch"
-    config_flags="$config_flags"
+build_deps="bison build-essential curl flex gcc libc-dev libevent-dev libexpat1-dev libnghttp2-dev make"
+apt-get update && apt-get install -y --no-install-recommends \
+  $build_deps \
+  bsdmainutils \
+  ca-certificates \
+  ldnsutils \
+  libevent-2.1-7 \
+  libexpat1 \
+  libprotobuf-c-dev \
+  protobuf-c-compiler
+curl -sSL $UNBOUND_DOWNLOAD_URL -o unbound.tar.gz
+echo "${UNBOUND_SHA256} *unbound.tar.gz" | sha256sum -c -
+tar xzf unbound.tar.gz
+rm -f unbound.tar.gz
+cd unbound-1.25.1
+groupadd _unbound
+useradd -g _unbound -s /usr/sbin/nologin -d /opt/unbound _unbound
+if [ $TARGETPLATFORM == "linux/amd64" ]; then
+    config_flags="--host=x86_64-pc-linux-gnu "
+elif [ $TARGETPLATFORM == "linux/arm64"  ]; then
+    config_flags="--host=armv7l-pc-linux-gnu "
 elif [ $TARGETPLATFORM == "linux/arm/v7" ]; then
-    echo "Building configure flags for 32bit arch"
-    config_flags="$config_flags"
+    config_flags="--host=armv7l-pc-linux-gnu "
 else
     echo "Unsupported target/build arch!"
     exit 1
 fi
+./configure \
+    --prefix=/opt/unbound \
+    --with-pthreads \
+    --with-username=_unbound \
+    --with-ssl=/opt/openssl \
+    --with-libevent \
+    --with-libnghttp2 \
+    --enable-dnstap \
+    --enable-tfo-server \
+    --enable-tfo-client \
+    --enable-event-api \
+    --enable-subnet \
+    $config_flags
+make install
+mv /opt/unbound/etc/unbound/unbound.conf /opt/unbound/etc/unbound/unbound.conf.example
+apt-get purge -y --auto-remove \
+  $build_deps
